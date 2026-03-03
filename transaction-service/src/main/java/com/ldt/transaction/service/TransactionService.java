4e15feb044ed86c62dbdd4fa1cc96bde3272f62f
LeDanhTien <danhtien2k4@gmail.com>
2026-03-03 16:06:18 +0700

    feat: using gRPC for transantion service call wallet service

--- transaction-service/src/main/java/com/ldt/transaction/service/TransactionService.java
+++ transaction-service/src/main/java/com/ldt/transaction/service/TransactionService.java
@@ -2,15 +2,17 @@ package com.ldt.transaction.service;
 
 import com.ldt.transaction.dto.TransactionResponse;
 import com.ldt.transaction.dto.TransferRequest;
-import com.ldt.transaction.dto.transfer.WalletTransferRequest;
 import com.ldt.transaction.dto.user.UserInternalResponse;
 import com.ldt.transaction.mapper.TransactionMapper;
 import com.ldt.transaction.model.Transaction;
 import com.ldt.transaction.model.TransactionStatus;
 import com.ldt.transaction.model.TransactionType;
 import com.ldt.transaction.repository.TransactionRepository;
+import com.ldt.wallet.grpc.WalletServiceGrpc;
+import com.ldt.wallet.grpc.WalletTransferRequest;
+import com.ldt.wallet.grpc.WalletTransferResponse;
 import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -19,16 +21,24 @@ import org.springframework.web.client.RestTemplate;
 import java.util.UUID;
 
 @Service
-@RequiredArgsConstructor
 public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final RestTemplate restTemplate;
     private final TransactionMapper transactionMapper;
-    @Value("${service.wallet-service.url}")
-    private String walletServiceUrl;
+
+    @GrpcClient("wallet-service")
+    private WalletServiceGrpc.WalletServiceBlockingStub walletServiceStub;
 
     @Value("${user-service.url}")
     private String userServiceUrl;
+
+    public TransactionService(TransactionRepository transactionRepository,
+                              RestTemplate restTemplate,
+                              TransactionMapper transactionMapper) {
+        this.transactionRepository = transactionRepository;
+        this.restTemplate = restTemplate;
+        this.transactionMapper = transactionMapper;
+    }
 
     @Transactional
     public TransactionResponse transfer(TransferRequest transferRequest) {
@@ -36,7 +46,7 @@ public class TransactionService {
         if (transactionRepository.existsByRequestId(transferRequest.getRequestId())) {
             throw new RuntimeException("Giao dịch đang được xử lí");
         }
-        //
+        // Gọi user-service qua REST (giữ nguyên)
         ResponseEntity<UserInternalResponse> responseFromUser =
                 restTemplate.getForEntity(
                         userServiceUrl + "/internal/users/" + transferRequest.getFromPhoneNumber(),
@@ -54,16 +64,14 @@ public class TransactionService {
         if (toUser.getStatus().equals("LOCKED")) {
             throw new RuntimeException("Ví bị khóa. Vui lòng nhập lại số điện thoại");
         }
-        // call wallet-service
-        WalletTransferRequest walletReq = new WalletTransferRequest();
-        walletReq.setFromUserId(fromUser.getUserId());
-        walletReq.setToUserId(toUser.getUserId());
-        walletReq.setAmount(transferRequest.getAmount());
-        restTemplate.postForEntity(
-                walletServiceUrl + "/internal/wallets/transfer",
-                walletReq,
-                Void.class
-        );
+        // Gọi wallet-service qua gRPC
+        WalletTransferRequest walletReq = WalletTransferRequest.newBuilder()
+                .setFromUserId(fromUser.getUserId().toString())
+                .setToUserId(toUser.getUserId().toString())
+                .setAmount(transferRequest.getAmount().toPlainString())
+                .build();
+        WalletTransferResponse walletResponse = walletServiceStub.transfer(walletReq);
+
         Transaction transaction = new Transaction();
         transaction.setRequestId(transferRequest.getRequestId());
         transaction.setFromWalletId(fromUser.getUserId());
@@ -79,3 +87,4 @@ public class TransactionService {
         return transactionMapper.toTransactionResponse(transaction);
     }
 }
+
