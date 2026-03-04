6ae46a9ffc6e564f5113fefdd6acc7df3f9eabe1
LeDanhTien <danhtien2k4@gmail.com>
2026-03-04 09:54:03 +0700

    feat: add internal key for grpc call

---
+++ wallet-service/src/main/java/com/ldt/wallet/grpc/auth/GrpcServerAuthInterceptor.java
@@ -0,0 +1,32 @@
+package com.ldt.wallet.grpc.auth;
+
+import io.grpc.*;
+import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@GrpcGlobalServerInterceptor
+public class GrpcServerAuthInterceptor implements ServerInterceptor {
+
+    @Value("${internal.secret}")
+    private String INTERNAL_KEY;
+
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
+            ServerCall<ReqT, RespT> serverCall,
+            Metadata metadata,
+            ServerCallHandler<ReqT, RespT> serverCallHandler) {
+
+        String key = metadata.get(
+                Metadata.Key.of("X-internal-key", Metadata.ASCII_STRING_MARSHALLER));
+
+        if (!INTERNAL_KEY.equals(key)) {
+            serverCall.close(Status.UNAUTHENTICATED.withDescription("Invalid key"), metadata);
+            return new ServerCall.Listener<>() {
+            };
+        }
+
+        return serverCallHandler.startCall(serverCall, metadata);
+    }
+}
